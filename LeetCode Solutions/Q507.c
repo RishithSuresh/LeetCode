146b9df40ae4a6791fcd8c67cb3ac23a3d033015
RishithSuresh <143060476+RishithSuresh@users.noreply.github.com>
2025-07-28 21:13:10 +0400

    Create Q507.c

---
+++ LeetCode Solutions/Q507.c
@@ -0,0 +1,16 @@
+bool checkPerfectNumber(int num) 
+{
+    int i,sum=0;
+    for(i=1; i<=(num/2); i++)
+    {
+        if(num%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if(sum == num)
+    {
+        return true;
+    }
+    return false;
+}
